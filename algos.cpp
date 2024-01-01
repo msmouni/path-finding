610085615a2e5eed53e5ec1c0ff51a6d620d0a83
msmouni <marwan-smouni@hotmail.fr>
2024-01-01 11:42:47 +0100

    Adding Algos Enum

---
+++ algos.cpp
@@ -0,0 +1,31 @@
+#include "algos.h"
+
+PathFindingAlgos getAlgoFromInt(int algo)
+{
+    switch (algo)
+    {
+    case 1:
+        return PathFindingAlgos::Bfs;
+    case 2:
+        return PathFindingAlgos::Dijkstra;
+    case 3:
+        return PathFindingAlgos::Astar;
+    default:
+        return PathFindingAlgos::None;
+    }
+}
+
+QString getAlgoName(int algo)
+{
+    switch (algo)
+    {
+    case 1:
+        return "BFS";
+    case 2:
+        return "Dijkstra";
+    case 3:
+        return "A*";
+    default:
+        return "";
+    }
+}
